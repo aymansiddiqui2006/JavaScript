1d522a720c71990dbdb71e26489620ba6dde9026
Your Nameaymansiddiqui2006 <aymansiddiqui2006@gmail.com>
2025-11-28 00:14:12 +0530

    parent_child explaination

---
+++ parent_child_sibling/parent_child_sibling.js
@@ -0,0 +1,7 @@
+const box=document.querySelector(".container")
+box.children[1].style.backgroundColor="grey"
+
+//here we are having child , parent , and sibling
+//in which we have to get the element through children , childNodes and child firstElementChild
+//where children or childNodes mean it will consider evrything the first xhild could be the text section between 2 div 
+//but the ElementChild will give the element like div span etc.. 
