71f144d2ca5990d10f82ed7892ba8d62696b6c38
Your Nameaymansiddiqui2006 <aymansiddiqui2006@gmail.com>
2025-12-04 14:07:12 +0530

    correcction in promise

--- promise2.js
+++ promise2.js
@@ -10,21 +10,21 @@
 // })
 const pro2 = new Promise((resolve, reject) => {
     setInterval(() => {
-        const num = Math.random();
-        if (num < 0.5) {
-            reject()
+        const num = Math.random()*10;
+        if (num < 9) {
+            reject(num)
         }
         else {
             console.log(`resolved ${num}`);
-            resolve()
+            resolve(num)
         }
     }, 1000);
 })
 
-.then(()=>{
-    console.log("resolved the number");
+.then((e)=>{
+    console.log(`resolved the number ${e}`);
     
 }).catch ((e)=>{
-    console.log(`negetive num`);
+    console.log(`nagetive num ${e}`);
     
 })
